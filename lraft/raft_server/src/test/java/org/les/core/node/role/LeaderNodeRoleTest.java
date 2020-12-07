94a37a40f32934539bff7c73e643ee13d2ab5efd
leleyi <653574281@qq.com>
2020-12-07 21:42:56 +0800

    add test for Role

---
+++ lraft/raft_server/src/test/java/org/les/core/node/role/LeaderNodeRoleTest.java
@@ -0,0 +1,47 @@
+package org.les.core.node.role;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.les.core.node.NodeId;
+import org.les.core.schedule.ElectionTimeout;
+import org.les.core.schedule.LogReplicationTask;
+
+public class LeaderNodeRoleTest {
+    @Test
+    public void testGetNameAndLeaderId() {
+        LeaderNodeRole role = new LeaderNodeRole(1, LogReplicationTask.NONE);
+        RoleNameAndLeaderId state = role.getNameAndLeaderId(NodeId.of("A"));
+        Assert.assertEquals(RoleName.LEADER, state.getRoleName());
+        Assert.assertEquals(NodeId.of("A"), state.getLeaderId());
+    }
+
+    @Test
+    public void testGetState() {
+        LeaderNodeRole role = new LeaderNodeRole(1, LogReplicationTask.NONE);
+        RoleState state = role.getState();
+        Assert.assertEquals(RoleName.LEADER, state.getRoleName());
+        Assert.assertEquals(1, state.getTerm());
+    }
+
+    @Test
+    public void testStateEquals() {
+        LeaderNodeRole role1 = new LeaderNodeRole(1, LogReplicationTask.NONE);
+        LeaderNodeRole role2 = new LeaderNodeRole(1, LogReplicationTask.NONE);
+        Assert.assertTrue(role1.stateEquals(role2));
+    }
+
+    @Test
+    public void testStateEqualsDifferentTerm() {
+        LeaderNodeRole role1 = new LeaderNodeRole(1, LogReplicationTask.NONE);
+        LeaderNodeRole role2 = new LeaderNodeRole(2, LogReplicationTask.NONE);
+        Assert.assertFalse(role1.stateEquals(role2));
+    }
+
+    @Test
+    public void testStateEqualsDifferentRoleName() {
+        LeaderNodeRole role1 = new LeaderNodeRole(1, LogReplicationTask.NONE);
+        CandidateNodeRole role2 = new CandidateNodeRole(2, ElectionTimeout.NONE);
+        Assert.assertFalse(role1.stateEquals(role2));
+    }
+
+}
