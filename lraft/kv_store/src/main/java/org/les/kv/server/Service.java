04bb386b813fd081b03f09624a12558c5f2d4dec
leleyi <653574281@qq.com>
2021-01-03 21:38:41 +0800

    kv_server message

--- lraft/kv_store/src/main/java/org/les/kv/server/Service.java
+++ lraft/kv_store/src/main/java/org/les/kv/server/Service.java
@@ -4,8 +4,19 @@ import org.les.core.node.Node;
 import org.les.kv.message.CommandRequest;
 import org.les.kv.message.GetCommand;
 import org.les.kv.message.SetCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class Service {
+    private static final Logger logger = LoggerFactory.getLogger(Service.class);
+
+    private final ConcurrentMap<String, CommandRequest<?>> pendingCommands = new ConcurrentHashMap<>();
+    private Map<String, byte[]> map = new HashMap<>();
 
     private final Node node;
 
@@ -14,7 +25,6 @@ public class Service {
     }
 
     public void set(CommandRequest<SetCommand> setCommandCommandRequest) {
-
     }
 
     public void get(CommandRequest<GetCommand> getCommandCommandRequest) {
