b51595e517ad05cd43c580725282de274d88733f
leleyi <653574281@qq.com>
2021-07-04 21:22:38 +0800

    kv_server

--- lraft/kv_store/src/main/java/org/les/kv/server/Server.java
+++ lraft/kv_store/src/main/java/org/les/kv/server/Server.java
@@ -43,7 +43,6 @@ public class Server {
                                 .addLast(new ServiceHandler(service));
                     }
                 });
-
         serverBootstrap.bind(this.port);
     }
 
